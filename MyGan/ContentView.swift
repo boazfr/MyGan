5ad7bea7444f2afa3dba2786975717c46571276b
Boaz Froog <boaz.froog@gmail.com>
2023-12-28 15:26:39 +0200

    add initial login screen template

--- MyGan/ContentView.swift
+++ MyGan/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var phoneNumber = ""
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Phone Number", text: $phoneNumber)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button("Login with Phone Number") {
+                //TODO: implement phone number auth
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(8)
+            
+            Divider().padding()
+            
+            TextField("Email", text: $email)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            SecureField("Password", text: $password)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Login with Gmail") {
+                //TODO: implement Google auth
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.red)
+            .cornerRadius(8)
         }
         .padding()
     }
