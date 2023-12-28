f9f369e07f9fb61b34d12efee2fcce792d28366e
Boaz Froog <boaz.froog@gmail.com>
2023-12-28 15:12:27 +0200

    Initial Commit

---
+++ MyGan/MyGanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGanApp.swift
+//  MyGan
+//
+//  Created by Boaz Froog on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyGanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
