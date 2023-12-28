33c9604f9ec69ab3146c3ccb9637ca7cec3023d0
Boaz Froog <boaz.froog@gmail.com>
2023-12-28 15:57:33 +0200

    add some animation for login screen

--- MyGan/ContentView.swift
+++ MyGan/ContentView.swift
@@ -11,14 +11,28 @@ struct ContentView: View {
     @State private var phoneNumber = ""
     @State private var email = ""
     @State private var password = ""
+    @State private var selectedLoginMethod: LoginMethod?
+    
+    enum LoginMethod {
+        case phoneNumber
+        case gmail
+    }
     
     var body: some View {
         
         VStack {
-            TextField("Phone Number", text: $phoneNumber)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Text("MyGan")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.top, 30)
+            
+            Spacer()
+            
             Button("Login with Phone Number") {
+                withAnimation {
+                    selectedLoginMethod = .phoneNumber
+                }
+                
                 //TODO: implement phone number auth
             }
             .padding()
@@ -26,23 +40,40 @@ struct ContentView: View {
             .background(Color.blue)
             .cornerRadius(8)
             
+            if selectedLoginMethod == .phoneNumber {
+                TextField("Phone Number", text: $phoneNumber)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .transition(.slide)
+            }
+            
             Divider().padding()
             
-            TextField("Email", text: $email)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            SecureField("Password", text: $password)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
             Button("Login with Gmail") {
+                withAnimation {
+                    selectedLoginMethod = .gmail
+                }
+                
                 //TODO: implement Google auth
             }
             .padding()
             .foregroundColor(.white)
             .background(Color.red)
             .cornerRadius(8)
+            
+            if selectedLoginMethod == .gmail {
+                TextField("Email", text: $email)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .transition(.opacity)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .transition(.opacity)
+            }
+            
+            Spacer()
         }
         .padding()
     }
